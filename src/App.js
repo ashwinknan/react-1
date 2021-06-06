482f96df878bb2bb4b72f73e2eca627bd48e82d9
ashwinknan <9294975+ashwinknan@users.noreply.github.com>
2021-06-06 12:48:24 +0530

    Components Part 2 - Render a clicked dish with cards at bottom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
-import { render } from '@testing-library/react';
-
-function App() {
+import { DISHES } from './shared/dishes';
 
 
-  return (
-    <div className="App">
-      <Navbar dark color = "primary">
-        <div className = "container">
-          <NavbarBrand href = "/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Navbar dark color = "primary">
+          <div className = "container">
+            <NavbarBrand href = "/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu  dishes = {this.state.dishes}/>
+      
+      </div>
+    );
+  }
+ 
   
 }
 
