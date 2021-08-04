001e0d45a16efb98bd9ca71d064bf348f7200acb
ashwinknan <9294975+ashwinknan@users.noreply.github.com>
2021-08-04 10:10:42 +0530

    React Router Changes for Navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
+import './App.css'
+import {BrowserRouter} from 'react-router-dom';
 
 
 
@@ -8,11 +10,12 @@ class App extends Component {
   
   render(){
     return (
-      <div>
-       
-        <Main />
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
       
-      </div>
     );
   }
  
